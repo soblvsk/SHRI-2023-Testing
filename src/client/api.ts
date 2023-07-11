0d16e6e519133b2172ad3f2a691ed0c9abd04585
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-11 11:47:02 +0700

    update refactor code style client

--- src/client/api.ts
+++ src/client/api.ts
@@ -2,36 +2,34 @@ import axios from 'axios';
 import { CartState, CheckoutFormData, CheckoutResponse, Product, ProductShortInfo } from '../common/types';
 
 export class ExampleApi {
-    constructor(private readonly basename: string) {
+  constructor(private readonly basename: string) {}
 
-    }
+  async getProducts() {
+    return await axios.get<ProductShortInfo[]>(`${this.basename}/api/products`);
+  }
 
-    async getProducts() {
-        return await axios.get<ProductShortInfo[]>(`${this.basename}/api/products`);
-    }
+  async getProductById(id: number) {
+    return await axios.get<Product>(`${this.basename}/api/products/${id}`);
+  }
 
-    async getProductById(id: number) {
-        return await axios.get<Product>(`${this.basename}/api/products/${id}`);
-    }
-
-    async checkout(form: CheckoutFormData, cart: CartState) {
-        return await axios.post<CheckoutResponse>(`${this.basename}/api/checkout`, { form, cart });
-    }
+  async checkout(form: CheckoutFormData, cart: CartState) {
+    return await axios.post<CheckoutResponse>(`${this.basename}/api/checkout`, { form, cart });
+  }
 }
 
 export const LOCAL_STORAGE_CART_KEY = 'example-store-cart';
 
 export class CartApi {
-    getState(): CartState {
-        try {
-            const json = localStorage.getItem(LOCAL_STORAGE_CART_KEY);
-            return JSON.parse(json) as CartState || {};
-        } catch {
-            return {};
-        }
+  getState(): CartState {
+    try {
+      const json = localStorage.getItem(LOCAL_STORAGE_CART_KEY);
+      return (JSON.parse(json) as CartState) || {};
+    } catch {
+      return {};
     }
+  }
 
-    setState(cart: CartState) {
-        localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cart));
-    }
+  setState(cart: CartState) {
+    localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cart));
+  }
 }
