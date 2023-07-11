5b34326abb789cfd3d8f3967ef6b5b366e5dcd4b
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-11 11:46:49 +0700

    update refactor code style pages

--- src/client/pages/Product.tsx
+++ src/client/pages/Product.tsx
@@ -8,25 +8,28 @@ import { ProductDetails } from '../components/ProductDetails';
 import { ApplicationState, productDetailsLoad } from '../store';
 
 export interface ProductRouteParams {
-    id: string;
+  id: string;
 }
 
 const bem = cn('Product');
 
 export const Product: React.FC = () => {
-    const dispatch = useDispatch();
-    const params = useParams<ProductRouteParams>();
-    const id = Number(params.id);
+  const dispatch = useDispatch();
+  const params = useParams<ProductRouteParams>();
+  const id = Number(params.id);
 
-    useEffect(() => {
-        dispatch(productDetailsLoad(id));
-    }, []);
+  useEffect(() => {
+    dispatch(productDetailsLoad(id));
+  }, []);
 
-    const product = useSelector((s: ApplicationState) => s.details[id]);
+  const product = useSelector((s: ApplicationState) => s.details[id]);
 
-    const content: React.ReactNode = product ?
-        <ProductDetails product={product}/> :
-        'LOADING';
+  const content: React.ReactNode = product ? <ProductDetails product={product} /> : 'LOADING';
 
-    return <div className={bem()}><Helmet title={product?.name} />{content}</div>;
-}
+  return (
+    <div className={bem()}>
+      <Helmet title={product?.name} />
+      {content}
+    </div>
+  );
+};
